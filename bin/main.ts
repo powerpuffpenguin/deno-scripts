e8111f55b8f0262fc311fd93cd98f8e39546992b
zuiwuchang <zuiwuchang@gmail.com>
2022-11-25 16:44:48 +0800

    test

---
+++ bin/main.ts
@@ -0,0 +1,13 @@
+import { Chan } from "easyts/core.ts";
+
+const c = new Chan<number>();
+(async () => {
+  for (let i = 0; i < 5; i++) {
+    await c.write(i);
+  }
+  await c.close();
+})();
+
+for await (const v of c) {
+  console.log(v);
+}
